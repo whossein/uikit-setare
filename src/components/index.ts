106c690e8430fc0b017d6fb0cf6392270d3e755b
h.aghatabar lenovo <whossein@hotmail.com>
2024-01-14 15:13:29 +0330

    fix build issues

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
-export { default as Button2 } from "./Button2";
 export { default as Input } from "./Input";
+export { default as Base } from "./base";
+export { default as ThemeProvider } from "./theme-provider";
