213d1e2bcbb4fbcd3c99df3ce2347c3e839fe9bf
h.aghatabar lenovo <whossein@hotmail.com>
2024-01-16 10:45:35 +0330

    fix build json issue

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import json from "@rollup/plugin-json";
 
 const packageJson = require("./package.json");
 
@@ -26,6 +27,7 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
+      json(),
       typescript({ tsconfig: "./tsconfig.json" }),
       terser(),
     ],
