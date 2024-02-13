5f0095e6300c60329a3badeeae2364d0fdc5e934
h.aghatabar lenovo <whossein@hotmail.com>
2024-02-13 12:31:05 +0330

    fix image component build issue

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@ import dts from "rollup-plugin-dts";
 import terser from "@rollup/plugin-terser";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import json from "@rollup/plugin-json";
+// import copy from "rollup-plugin-copy";
+// import images from "@rollup/plugin-image";
+// import url from "@rollup/plugin-url";
 
 const packageJson = require("./package.json");
 
@@ -26,6 +29,30 @@ export default [
     plugins: [
       peerDepsExternal(),
       resolve(),
+      // copy({
+      //   targets: [
+      //     // { src: 'assets/images/**/*', dest: 'dist/public/images' },
+      //     {
+      //       src: "src/components/image/holder-images",
+      //       dest: "dist/",
+      //     },
+      //   ],
+      // }),
+
+      // images(),
+      // url({
+      //   // Where to put files
+      //   destDir: "dist/assets/",
+      //   // Path to put infront of files (in code)
+      //   publicPath:
+      //     process.env.NODE_ENV === "development"
+      //       ? "http://localhost:8000/dist/assets/"
+      //       : "./assets/",
+      //   // File name once copied
+      //   fileName: "[name][extname]",
+      //   // Kinds of files to process
+      //   include: ["**/*.svg", "**/*.png", "**/*.gif", "**/*.jpg", "**/*.jpeg"],
+      // }),
       commonjs(),
       json(),
       typescript({ tsconfig: "./tsconfig.json" }),
